1b63790196e753ae19f242bb4a52ead268c3b4e5
Mario Reder <mreder1289@gmail.com>
2019-12-10 16:55:52 +0100

    color variables

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -1,11 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/explicit-member-accessibility */
+const COLOR_BROWN = '#611821';
+const COLOR_GREEN = '#009784';
+const COLOR_LIGHT_GREEN = '#90f0e5';
+const COLOR_LIGHT_YELLOW = '#fef3d3';
+const COLOR_LIGHT_LIGHT_YELLOW = '#fffdee';
+const COLOR_YELLOW = '#ffcf00';
+
 export type Themes = DefaultTheme;
 
 export interface Theme {
   background: string;
 }
 
-/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 export class DefaultTheme implements Readonly<Theme> {
-  background = '#ffcf00';
+  background = COLOR_YELLOW;
 }
 /* eslint-enable */
